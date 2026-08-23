bf624a9970ec1c230073460c970e00994b3dad03
Martin <uzunovm0@gmail.com>
2026-08-24 01:14:59 +0300

    initial working version

--- github-analyzer/frontend/src/app/app.component.ts
+++ github-analyzer/frontend/src/app/app.component.ts
@@ -1,25 +1,32 @@
 import { Component } from '@angular/core';
 import { GithubService } from './github.service';
 
-
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
 })
-
 export class AppComponent {
+  githubUsername = '';
+  data: any;
+  error = '';
 
-    githubUsername = '';
-    data: any;
-    error = "";
+  constructor(private githubService: GithubService) {}
 
-    constructor(private githubService: GithubService) {}
-
-    search() {
+  search() {
     /**
      * TODO: call backend, handle logic
      */
-    }
+    this.error = '';
+    this.data = null;
 
+    this.githubService.getUser(this.githubUsername).subscribe({
+      next: (response) => {
+        this.data = response;
+      },
+      error: (err) => {
+        this.error = err.error?.detail || 'Something went wrong.';
+      },
+    });
+  }
 }
